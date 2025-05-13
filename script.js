fe7a04fe99ae7c30573f4258290dac7dc34a946f
Luca Campion <luca.campion07@gmail.com>
2025-05-13 15:48:19 +0200

    Implementazione pulsante percorso Maps

--- script.js
+++ script.js
@@ -11,15 +11,23 @@ async function start() {
     // carica json
     await loadJson('db.json');
 
-
     // crea i pin
     let markers = [];
+    const pos = await getLocalCoordinates();
     for (let i = 0; i < jsonData.length; i++) {
-        markers[i] = L.marker([jsonData[i].location_lat, jsonData[i].location_lon]).addTo(map);
+        markers[i] = L.marker([jsonData[i].location_lat, jsonData[i].location_lon]).addTo(map).bindPopup(
+            '<h1 id="id_pin">' + jsonData[i].id + '</h1>' + 
+            '<p>Coordinate: ' + jsonData[i].location_lat + ', ' + jsonData[i].location_lon + '</p>' + 
+            '<p>Stato: ' + jsonData[i].status + '</p>' + 
+            '<p>Operativo: ' + jsonData[i].operative + '</p>' +
+            '<a href=\'https://www.google.com/maps?saddr=' + pos[0] + ',' + pos[1] + '&daddr=' + jsonData[i].location_lat + ',' + jsonData[i].location_lon + '\'><button>Apri in Maps</button></a>'
+        );
+        /*
         markers[i].options.id = jsonData[i].id;
         markers[i].on('click', () => {
             clickOnPin(jsonData[i].id);
         });
+        */
     }
 }
 
@@ -28,6 +36,7 @@ async function loadJson(path) {
     jsonData = await response.json();
 }
 
+/*
 function clickOnPin(pinID) {
     alert('Pin #' + pinID +
         '\nCoordinate: ' + jsonData.find(pin => pin.id === pinID).location_lat + ', ' + jsonData.find(pin => pin.id === pinID).location_lon +
@@ -35,8 +44,8 @@ function clickOnPin(pinID) {
         '\nOperativo: ' + jsonData.find(pin => pin.id === pinID).operative
         //https://www.google.com/maps?saddr=[coord attuali]&daddr=[coord idrante]
     );
-
 }
+*/
 
 async function getLocalCoordinates() {
     return new Promise((resolve, reject) => {
